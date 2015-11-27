31e1be270eedbf9953e559e8c299df52d50ccab7
Gary Burgess <gary.burgess@gmail.com>
2015-11-27 11:52:29 +0000

    Add require-path option to bundle

--- src/GulpPurescript/Options.purs
+++ src/GulpPurescript/Options.purs
@@ -99,6 +99,7 @@ newtype PscBundle
               , "module" :: NullOrUndefined (Either String (Array String))
               , main :: NullOrUndefined (Either Boolean String)
               , namespace :: NullOrUndefined String
+              , requirePath :: NullOrUndefined String
               }
 
 newtype PscDocs
@@ -148,11 +149,13 @@ instance isForeignPscBundle :: IsForeign PscBundle where
                    , "module": _
                    , main: _
                    , namespace: _
+                   , requirePath: _
                    } <$> (readProp srcKey obj >>= readEither)
                      <*> readProp outputKey obj
                      <*> (readProp moduleKey obj >>= readEitherNU)
                      <*> (readProp mainKey obj >>= readEitherNU)
-                     <*> readProp namespaceKey obj)
+                     <*> readProp namespaceKey obj
+                     <*> readProp requirePathKey obj)
 
 instance isForeignPscDocs :: IsForeign PscDocs where
   read obj =
